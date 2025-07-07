1e16a73b29f4c202fc21f5ca041a75e48be61591
Xiaofang Wang <xiaofangwxf@gmail.com>
2025-07-07 11:36:16 -0400

    Add LinkedIn link

--- ui/src/components/FooterNote.jsx
+++ ui/src/components/FooterNote.jsx
@@ -1,10 +1,19 @@
 import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 export function FooterNote() {
   return (
     <Box mt={10} textAlign="right">
       <Typography variant="body2" color="textSecondary">
-        Developed by Xiaofang Wang
+        Developed by{" "}
+        <Link
+          href="https://www.linkedin.com/in/xiaofang-wang-09789b11/"
+          target="_blank"
+          rel="noopener"
+          underline="hover"
+        >
+          Xiaofang Wang
+        </Link>
       </Typography>
     </Box>
   );
